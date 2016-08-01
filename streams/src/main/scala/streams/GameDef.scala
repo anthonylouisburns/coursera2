1074fbe2b8dac1bcdb7d0d44d5219ae84e71accf
burnstony <burnstony@gmail.com>
2016-08-01 01:35:21 -0400

    solution first draft

--- streams/src/main/scala/streams/GameDef.scala
+++ streams/src/main/scala/streams/GameDef.scala
@@ -5,7 +5,6 @@ import common._
 /**
  * This trait represents the layout and building blocks of the game
  *
- * @TODO: SHOULD RENAME `x` and `y` in class Pos to `row` and `col`. It's
  * confusing to have `x` being the vertical axis.
  */
 trait GameDef {
@@ -25,18 +24,18 @@ trait GameDef {
    *   1 o o o o
    *   2 o # o o    # is at position Pos(2, 1)
    *   3 o o o o
-   *
-   *   ^
+    *
+    *
    *   |
    *
    *   x axis
    */
-  case class Pos(x: Int, y: Int) {
+  case class Pos(row: Int, col: Int) {
     /** The position obtained by changing the `x` coordinate by `d` */
-    def dx(d: Int) = copy(x = x + d)
+    def dx(d: Int) = copy(row = row + d)
 
     /** The position obtained by changing the `y` coordinate by `d` */
-    def dy(d: Int) = copy(y = y + d)
+    def dy(d: Int) = copy(col = col + d)
   }
 
   /**
@@ -84,7 +83,7 @@ trait GameDef {
    * This function returns the block at the start position of
    * the game.
    */
-  def startBlock: Block = ???
+  def startBlock: Block = Block(startPos, startPos)
 
 
   /**
@@ -95,7 +94,7 @@ trait GameDef {
   case class Block(b1: Pos, b2: Pos) {
 
     // checks the requirement mentioned above
-    require(b1.x <= b2.x && b1.y <= b2.y, "Invalid block position: b1=" + b1 + ", b2=" + b2)
+    require(b1.row <= b2.row && b1.col <= b2.col, "Invalid block position: b1=" + b1 + ", b2=" + b2)
 
     /**
      * Returns a block where the `x` coordinates of `b1` and `b2` are
@@ -112,22 +111,22 @@ trait GameDef {
 
     /** The block obtained by moving left */
     def left = if (isStanding)         dy(-2, -1)
-               else if (b1.x == b2.x)  dy(-1, -2)
+               else if (b1.row == b2.row)  dy(-1, -2)
                else                    dy(-1, -1)
 
     /** The block obtained by moving right */
     def right = if (isStanding)        dy(1, 2)
-                else if (b1.x == b2.x) dy(2, 1)
+                else if (b1.row == b2.row) dy(2, 1)
                 else                   dy(1, 1)
 
     /** The block obtained by moving up */
     def up = if (isStanding)           dx(-2, -1)
-             else if (b1.x == b2.x)    dx(-1, -1)
+             else if (b1.row == b2.row)    dx(-1, -1)
              else                      dx(-1, -2)
 
     /** The block obtained by moving down */
     def down = if (isStanding)         dx(1, 2)
-               else if (b1.x == b2.x)  dx(1, 1)
+               else if (b1.row == b2.row)  dx(1, 1)
                else                    dx(2, 1)
 
 
@@ -135,22 +134,22 @@ trait GameDef {
      * Returns the list of blocks that can be obtained by moving
      * the current block, together with the corresponding move.
      */
-    def neighbors: List[(Block, Move)] = ???
+    def neighbors: List[(Block, Move)] = List((left,Left),(right,Right),(up,Up),(down,Down))
 
     /**
      * Returns the list of positions reachable from the current block
      * which are inside the terrain.
      */
-    def legalNeighbors: List[(Block, Move)] = ???
+    def legalNeighbors: List[(Block, Move)] = neighbors.filter(_._1.isLegal)
 
     /**
      * Returns `true` if the block is standing.
      */
-    def isStanding: Boolean = ???
+    def isStanding: Boolean = b1.row == b2.row && b1.col == b2.col
 
     /**
      * Returns `true` if the block is entirely inside the terrain.
      */
-    def isLegal: Boolean = ???
+    def isLegal: Boolean = terrain(b1) && terrain(b2)
   }
 }
